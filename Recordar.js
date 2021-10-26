7bf21fd5b5eedae8c96af2e9678c74eb3fd78ec1
ezequiel benavidez <vaneteamoa@gmail.com>
2021-10-26 20:10:47 -0300

    javascipt

--- Recordar.js
+++ Recordar.js
@@ -22,3 +22,77 @@
 // }
 
 // console.log(suma(2, 5));
+
+
+
+
+
+// var restar = (a, b) => {
+
+
+//     return a - b;
+// }
+
+// console.log(restar(2, 5));
+
+
+// o 
+
+
+// var restar = (a, b) => a - b;
+
+// console.log(restar(2, 5));
+
+
+
+
+
+//manipulación de arrelgos
+
+
+/*
+var letras = ['a', 'b', 'c', 'd']
+
+console.log(letras.length)
+
+for (let i = 0; i < letras.length; i++) {
+    const element = letras[i];
+    console.log(element);
+}
+
+letras.forEach((elemento) => console.log(elemento))
+*/
+
+//push - shift - pop
+
+/*
+var letras = ['a', 'b', 'c', 'd'];
+
+letras.push('f');
+
+console.log(letras);
+
+var primerElemento = letras.shift()
+
+console.log(letras);
+
+var ultimoElemento = letras.pop()
+
+console.log(ultimoElemento)
+
+console.log(letras)
+
+// map
+
+var estudiantes = ['Daniel', 'Giorno', 'Ezequiel', 'Gabriela'];
+
+var asistencia = estudiantes.map((nombres) => {
+    return {
+        nombres: nombres,
+        asistencia: false
+    }
+})
+
+console.log(estudiantes);
+console.log(asistencia);
+*/
